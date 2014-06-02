de21b7043d0b950109d7f82ca0b4333cddcd6529
FBW <fw@speedpoint.de>
2014-06-02 19:44:28 +0200

    aktualisierte Version vom Clonemaster DV0020 (Mai 2014)

--- DVBackup_mit_sync.sh
+++ DVBackup_mit_sync.sh
@@ -12,7 +12,7 @@
 # --> Anpassung der /etc/rsnapshot.conf nicht vergessen!
 #     Bitte passendes Beispiel conf Datei beachten (s.u.) 
 #
-# Sppedpoint nG GmbH (FW), Stand: Augunst 2013
+# Speedpoint nG GmbH (FW), Stand: Januar 2014
 
 
 
@@ -24,8 +24,7 @@ message=true                           # Desktop Benachrichtigung senden?
 interval=daily                         # [hourly/daily/weekly/monthly]  
 config=/etc/rsnapshot.spng.conf        # Optional eigene Konfigurationsdatei
 #
-volume=/dev/sdc1                       # Partition fuer rsnapshot Backups
-hdd=/mnt/snapshots                     # Mountpoint fuer Snapshot Partition
+snap=/dev/sdc1                         # Devicebezeichn. der Snapshot Partition
 hddout=true                            # nach rsnapshot aushaengen?
 #
 crypt=true                             # Verschluesselung auf dem ext. Medium?
@@ -33,13 +32,17 @@ dvkey=david                            # Passwort fuer Verschluesselung
 #
 media=rdx                              # Hier nur 'dds' oder 'rdx' einsetzen!
 godown=false                           # Shutdown nach Backup?
+reset=true                             # Reset nach Backup?
+#
+toolpfad=/install/skripte
+vcheck=false                           # Virensuche in trpword starten?
+vtool=$toolpfad/vcheck.sh              # Pfad zu virencheck.sh
+#
+rkcheck=true                           # Rootkit Suche starten?
+rktool=$toolpfad/rkcheck.sh            # Pfad zu rootkit.sh
 #
 mirror=true                            # Sync der Spiegelplatte vor dem Backup
-toolpfad=/install/skripte
-tool=mirror_hdd.sh                     # Pfad & Name des Sync Scripts
-#
-vrk=true                               # Rootkit Suche starten?
-vrktool=$toolpfad/vrk.sh               # Pfad zu vrk.sh
+tool=$toolpfad/mirror_hdd.sh           # Pfad & Name des Sync Scripts
 #
 #
 # ACHTUNG: Namen der Logdatei im Skript SaveAndHalt.bat des WinPCs abgleichen!
@@ -98,17 +101,57 @@ if [[ `cat $config | grep Speedpoint` = "" ]]; then
      exit 1
 fi
 
+# Mountpoint fuer die Snapshots testen:
+if [ "`cat /etc/fstab | grep $snap`" ]; then
+   mount $snap
+   if [ $? = "1" ]; then
+      text="Fehler beim Einhaengen der Sicherungsfestplatte $snap."
+      echo $text
+      echo $text | mail -s "WARNUNG: Backup nicht korrekt konfiguriert!" $user@localhost
+      exit 1
+      echo ""
+   else
+      umount $snap && sleep 3
+      # ~~~ neu ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      # Pruefen, ob Dateisystem von $snap sauber ist:
+      echo "Snapshot Medium ist $snap."
+      if [ "`e2fsck -n $snap | grep 'sauber'`" ]; then
+         sauber=true
+         fscheck="Das Dateisystem von $snap ist sauber."
+         echo $fscheck
+      else
+         sauber=false
+         text="WARNUNG: Das Dateisystem von $snap muss repariert werden, bitte umgehend Speedpoint anrufen."
+         echo $text | mail -s "Die Sicherungsfestlatte ist fehlerhaft und muss geprueft werden." $user@localhost
+         echo $text
+         echo ""
+         exit 1
+      fi
+      # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+   fi
+fi
 
 # Fuer Display Benachrichtigungen
-export DISPLAY=:0.0 ;
+ichbins=`whoami`
+export DISPLAY=:0.0
 export XAUTHORITY=$(/usr/bin/find /var/run/gdm -path "*$user*/database") ;
 
 
-# Schritt 0: Rootkit Suche -----------------------------------------------------
-if $vrk ; then
-   sh $vrktool
-fi   
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "ACHTUNG: Nach der Sicherung erfolgt u.U.ein Reset bzw. ein Systemstop!"
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo ""
 
+
+# Schritt 0: Rootkit & Virensuche ----------------------------------------------
+if $vcheck ; then
+   $vtool
+fi   
+#
+if $rkcheck ; then
+   $rktool
+fi   
 
 # Schritt 1: HDD 1 auf Spiegelplatte synchronisieren ---------------------------
 if $mirror ; then
@@ -116,9 +159,11 @@ if $mirror ; then
    if $message ; then
 	  su - $user -c "notify-send 'Spiegelplatte wird aktualisiert...' -i /usr/share/icons/gnome/32x32/actions/go-jump.png --hint=int:transient:1"
    fi
+
    echo "Spiegelplatte wird synchronisiert..."
    cd $toolpfad
-   sh $tool
+   $tool
+
    if [ ! $? -eq 0 ]; then
       echo "WARNUNG: Fehler beim Synchronisieren der Spiegelplatte!"
    fi
@@ -133,7 +178,7 @@ fi
 # rsnapshot HDD ggf. einhaengen
 mount | grep "on ${volume} type" > /dev/null
 if [ $? -ne 0 ]; then 
-   mount $volume &> /dev/null
+   mount $snap &> /dev/null
 fi
 
 # Startnachricht an User
@@ -147,7 +192,7 @@ cd /home/david
 
 # Snapshot erstellen & Logdatei fuer Fehler anlegen
 jetzt=`date`
-errorlog="$hdd/Fehler.log"
+errorlog="$snap/Fehler.log"
 rsnapshot -c $config -v $interval 2>>$errorlog
 
 if [ $? -eq 0 ]; then
@@ -160,40 +205,42 @@ fi
 cd $here
 
 # ggf. verschluesselte Kopie von daily.0 anlegen & verschluesseln
-data=$hdd/daily.0
+data=$snap/daily.0
 
 if $crypt ; then
-   rm -f $hdd/*.tar.nc 2>>/dev/null
+   rm -f $snap/*.tar.nc 2>>/dev/null
    keyfile=`mktemp /tmp/keyfile.XXXXXXXX`
    echo $dvkey >$keyfile
    echo "Verschluesselung vorbereiten..."
-   tar cf $hdd/daily.0.tar $hdd/daily.0
+   tar cf $snap/daily.0.tar $snap/daily.0 && echo OK
    echo "Verschluesseln..."
-   mcrypt -f `echo $keyfile` $hdd/daily.0.tar 2>>$errorlog
+   if $message ; then
+      su - $user -c "notify-send 'Verschluesseln... $crypt_message' -i /usr/share/icons/gnome/32x32/emblems/emblem-default.png --hint=int:transient:1"
+   fi
+   mcrypt -f `echo $keyfile` $snap/daily.0.tar 2>>$errorlog
    if [ $? -eq 0 ]; then
       crypt_message="Die gesicherten Daten wurden mit dem Passwort $dvkey verschluesselt"
       echo "Verschluesselung mit Passwort $dvkey erfolgreich."
-      data=$hdd/daily.0.tar.nc
+      data=$snap/daily.0.tar.nc
    else
       crypt_message="ACHTUNG: Bei der Verschluesselung ist ein Fehler aufgetreten, bitte $errorlog beachten!  "
       echo "WARNUNG: Fehler bei der Verschluesselung, bitte $errorlog beachten!"
    fi
-   rm -f $hdd/daily.0.tar
+   rm -f $snap/daily.0.tar
    rm -f $keyfile 
 else
    echo "Verschluesselung deaktiviert"
-   crypt_message="Die Verschluesselung der gesicherten Daten ist nicht aktiviert."
+   crypt_message="Die gesichterten Daten wurden nicht verschluesselt."
 fi
 
 if $message ; then
    if  $backup_success ; then
       # Erfolgsmeldung auf Desktop
-      su - $user -c "notify-send 'Backup erfolgreich. $crypt_message :-)' -i /usr/share/icons/gnome/32x32/emblems/emblem-default.png --hint=int:transient:1"
+      su - $user -c "notify-send 'Backup erfolgreich. $crypt_message' -i /usr/share/icons/gnome/32x32/emblems/emblem-default.png --hint=int:transient:1"
    else
       su - $user -c "notify-send 'WARNUNG: Backup mit Fehlern beendet $crypt_message :-(    Bitte $errorlog beachten!' -i /usr/share/icons/gnome/32x32/status/dialog-error.png"
    fi
 fi
-
 
 # Schritt 3: Externe Sicherung -------------------------------------------------
 
@@ -206,16 +253,20 @@ if $message ; then
 fi
 
 chmod 0666 $device
+
 tar -cvf $device $data >>$tempfile 2>&1
 if [ $? -eq 0 ]; then
    info="erfolgreich"
    echo "Externe Sicherung okay."
-   echo "Externe Datensicherung von $jetzt erfolgreich abgeschlossen. $crypt_message :-)" | mail -s "Externe Datensicherung erfolgreich :-)" $user@localhost
 else
-   info="MIT FEHLERN"
+   info="--> FEHLERHAFT <--"
    echo "WARNUNG: Externe Datensicherung fehlerhaft."
    cat $tempfile | mail -s "WARNUNG: Externe Datensicherung fehlerhaft!" $user@localhost
 fi
+
+echo "Die Externe Datensicherung von $jetzt wurde $info abgeschlossen. $crypt_message. $fscheck" | mail -s "Externe Datensicherung $info" $user@localhost
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 # Medienauswurf
 $remove
@@ -223,9 +274,9 @@ $remove
 rm -f $tempfile
 
 # HDD ggf. aushaengen
-if $hddout ; then
-   cd && sleep 5	
-   umount $volume &> /dev/null
+if $snapout ; then
+   cd / && sleep 5	
+   umount $snap &> /dev/null
 fi
 
 # Backup Protokoll des/der PA/DMP PCs als Mail versenden, falls vorhanden:
@@ -237,17 +288,20 @@ if [ -r $padmplog ]; then
    fi
 fi
 
-if $godown ; then
-   ansage="Das System schaltet in 20 Sekunden ab..."
-   echo $ansage && sleep 20
-   /sbin/shutdown -h now
-fi
 echo ""
-
 if $message ; then
    su - $user -c "notify-send 'Externe Sicherung `echo $info` beendet. `echo $ansage`' -i /usr/share/icons/gnome/32x32/actions/go-jump.png --hint=int:transient:1"
 fi
 
+#export DISPLAY=`cat $ichbins/DisplayAusgabe`
+
+if $godown ; then
+   echo "Das System schaltet in 20 Sekunden ab..." && sleep 20
+   /sbin/shutdown -h now
+elif $reset ; then
+   echo "Das System startet in 120 Sekunden neu..." sleep 120
+   /sbin/shutdown -r now
+fi
 
 exit 0
 
@@ -282,7 +336,7 @@ cmd_logger       /usr/bin/logger
 #########################################
 #           BACKUP INTERVALS            #
 #########################################
-interval         daily   10
+interval         daily   20
 #
 verbose          2
 loglevel         3
